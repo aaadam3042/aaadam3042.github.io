1462195c74d78d9eb75cd7f3e179fcecd209a651
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2024-07-13 17:11:18 +1000

    Mass changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,157 @@ import './App.css'
 import './assets/fonts/AwalFont.css'
 import { AppBar, Box, Button, Container, Card, Chip, Divider, Grid, Paper, Stack, Toolbar, Typography } from '@mui/material'
 import Header from './components/Header'
-import cardBG from './assets/images/Cardbg.png'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function App() {
+
+  const projects = [
+    {
+      title: "Eyestrain Manager",
+      platform: "desktop",
+      description: "",
+      tags: ["python", "health"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+    {
+      title: "Personal website",
+      platform: "Web",
+      description: "You're looking at it right now",
+      tags: ["Javascript", "React", "MaterialUI"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+    {
+      title: "Expense Tracker",
+      platform: "IOS",
+      description: "",
+      tags: ["Swift", "SwiftUI"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+    {
+      title: "Halal Food Sydney App",
+      platform: "Web",
+      description: "",
+      tags: ["Javascript", "React", "Python", "Flask", "PostgreSQL"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+  ];
+
+  const games = [
+    {
+      title: "Pythonic Hangman",
+      platform: "desktop",
+      description: "",
+      tags: ["python", "game"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+    {
+      title: "Rusty Hangman",
+      platform: "macOS",
+      description: "",
+      tags: ["Rust", "game"],
+      image: "/",
+      githubURL: "/",
+      appURL: "",
+    },
+  ];
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+
+  const projectGallery = projects.map(project =>
+    <>
+    <Card>
+      {project.title}
+    </Card>
+    </>
+  )
+
+  const gameGallery = projects.map(game =>
+    <>
+    <Card>
+
+    </Card>
+    </>
+  )
+
+  const csel = {
+    'react-multi-carousel-list': {
+      position: 'unset !important',
+    },
+  }
+
   return (
     <>
-      <Header />
-      <Paper square elevation={5} sx={{width: '100vw', height: '100vh', top:0, left:0, right:0, bottom:0, margin:0, padding:0, marginTop: "45vh", bgcolor: "#1a181b"}}>
+      <Box id="BasePage">
+        <Header />
+        <Paper square elevation={5} sx={{width: '100vw', height: '100vh', top:0, left:0, right:0, bottom:0, 
+            margin:0, padding:0, marginTop: "100vh;", bgcolor: "#1a181b", display: 'flex', flexDirection: 'column',
+            justifyContent:'start', alignItems:'center'
+            }}>
+        
+            <Box width='90vw' mt={5} mb={10}>
+                <Typography color='white' variant='h4'> About Me </Typography>
+                <Box width='100%' display="flex" justifyContent="center" my={1}>
+                    <Divider variant='middle' sx={{bgcolor: 'white', width: '60vw'}}></Divider>
+                </Box>
+                    
+                <Typography color="#efeff0" variant='h5' style={{display: 'inline-block', whiteSpace: 'pre-line', wordWrap: 'break-word'}} align="center" paddingX={1}> 
+                Hi! I'm Ahmad. I'm an Honours year student at {}
+                <b style={{color: 'orange', whiteSpace: 'nowrap'}}> The University of Sydney </b>
+                {} studying a bachelors of Engineering (Software).
+                </Typography>
+            </Box>
 
-      <Typography color="#efeff0" variant='body1' style={{display: 'inline-block', whiteSpace: 'pre-line'}} align="justify" paddingX={2}> 
-       Hi! I'm Ahmad. I'm an Honours year student at 
-      <b style={{color: 'orange', whiteSpace: 'nowrap'}}> The University of Sydney </b>
-       studying a bachelors of Engineering (Software).
-      </Typography>
+            <Box width='90vw' height={300} bgcolor='red'>
+            <a name='projects'></a>
+            <Carousel style={""}
+                swipeable={false}
+                draggable={false}
+                showDots={true}
+                responsive={responsive}
+                ssr={false} // means to render carousel on server-side.
+                infinite={true}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+                renderDotsOutside>
+                {projectGallery}
+            </Carousel>
 
-      </Paper>
+            </Box>
+
+        </Paper>
+      </Box>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,70 @@
-import { Box, Button, Paper, Toolbar, Typography, Stack, styled } from '@mui/material'
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive'
+import { Box, Button, Divider, Paper, Toolbar, Typography, Stack, styled } from '@mui/material'
 import banner from '../assets/images/Banner4.jpg'
-import { LinkedIn, Email } from "@mui/icons-material"
+import { LinkedIn, Email, KeyboardArrowDown } from "@mui/icons-material"
 
 export function Header() {
     const Offset = styled('div')(({ theme }) => theme.mixins.Box);
 
+    const [showButton, setShowButton] = useState(true);
+
+	const isBigScreen = useMediaQuery({query: '(min-width: 1024px)'})
+    let scrollSpeed = "-1"
+    if (isBigScreen) {
+		scrollSpeed = "-2"
+	}		
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const speedFactor = scrollSpeed; // Adjust this factor to make the background scroll faster or slower
+            const backgroundElement = document.getElementById('background-box');
+            backgroundElement.style.backgroundPositionY = `${scrollPosition * speedFactor}px`;
+            console.log(scrollPosition)
+            if (scrollPosition > 250) {
+                setShowButton(false)
+            } else {
+                setShowButton(true)
+            }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
+
     return(
     <>
-        <Paper elevation={3} square sx={{bgcolor: 'black', position: "absolute", top: 0, left:0, right: 0, minHeight:300, width: "100vw", height: "40vh" , alignContent: "center", justifyContent: "center", alignItems: "center", alignSelf: "center", justifyItems: "center"}}>
-          <Box position="absolute" top="0px" bottom="0px" left="0px" right="0px" zIndex={1} style={{backgroundImage:`url(${banner})`, backgroundPosition: "center", backgroundSize: "cover", filter: "blur(0px)"}} />
-          <Stack direction="column" style={{position: "relative", zIndex: 3}} paddingTop={2}>
-            <Typography fontFamily='AwalRamadhan' color="white" variant='h1' textAlign="center" alignSelf="center" justifySelf="center" zIndex={3}> Ahmad Adam </Typography>
-            <Stack direction="row" position="relative" alignSelf="center" alignContent="center" >
-              <Button endIcon={<LinkedIn />} variant='contained' sx={{paddingX: 2, marginX: 2}} href='https://www.linkedin.com/in/ahmad-a-adam/'> Connect </Button>
-              <Button startIcon={<Email />} variant='contained' sx={{paddingX: 2, marginX: 2}}> Contact </Button>
+        <Paper elevation={3} square sx={{bgcolor: 'black', position: "absolute", top: 0, left:0, right: 0, 
+            width: "100vw", height: "100vh" , alignContent: "center", justifyContent: "center",
+            alignItems: "center", alignSelf: "center", justifyItems: "center"
+            }}>
+            <Box id="background-box" 
+                position="absolute" top="0px" bottom="0px" left="0px" right="0px" zIndex={1} 
+                style={{backgroundImage:`url(${banner})`, backgroundPosition: "center top", backgroundRepeat: "no-repeat",
+                backgroundSize: "300vh", backgroundAttachment: 'fixed', filter: "blur(0px)", willChange: 'transform'}
+            } />
+            <Stack direction="column" style={{position: "relative", zIndex: 3}} paddingTop={2} display='flex' alignContent='center'>
+                <Typography fontFamily='AwalRamadhan' color="white" variant='h1' textAlign="center" alignSelf="center" justifySelf="center" zIndex={4}> Ahmad Adam <span style={{whiteSpace: 'nowrap', fontSize: '65px'}}> 💻 </span> </Typography> 
+                <Box width='50vw' alignSelf='center'>
+                    <Divider variant="middle" sx={{bgcolor: 'white'}} />
+                </Box>
+                <Typography paddingY={2} fontWeight='bold' color="white"> Honours year <b style={{color: 'orange'}}> Software Engineering </b> student </Typography>
+                <Stack direction="row" position="relative" alignSelf="center" alignContent="center" >
+                <Button endIcon={<LinkedIn />} variant='contained' sx={{paddingX: 2, marginX: 2}} href='https://www.linkedin.com/in/ahmad-a-adam/'> Connect </Button>
+                <Button startIcon={<Email />} variant='contained' sx={{paddingX: 2, marginX: 2}}> Contact </Button>
+                </Stack>
             </Stack>
-          </Stack>
-        <Offset />
+
+            <Box position='fixed' bottom='20px' width='100vw' zIndex={5} display='flex' justifyContent='center'>
+                <Paper elevation={5} sx={{borderRadius: 5, textAlign: 'center', maxWidth:'170px', display:'flex', paddingX: 1,
+                     opacity: showButton ? 1 : 0, transition: 'opacity 0.3s ease-in-out'}}>
+                    <KeyboardArrowDown sx={{alignSelf: 'center'}} />
+                    <Button sx={{py: 1}} href="#projects"> Projects </Button>
+                </Paper>
+            </Box>
+            
+            <Offset />
         </Paper>
     </>
     )
