a8890d41de10e15a4cf9d804f8c6fc76f23c1915
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2024-08-08 09:43:21 +1000

    Fixed thumbnail import

--- src/assets/images/projects/index.js
+++ src/assets/images/projects/index.js
@@ -1,5 +1,6 @@
 import Default from './Default.png'
 import EyestrainManager from './Eyestrain-manager.png'
+import IStrainManager from './IStrain-manager.png'
 import HFS from './HFS.png'
 import PersonalWebsite from './Personal-website.png'
 import PythonicHangman from './Pythonic-hangman.png'
@@ -8,6 +9,7 @@ import RustyHangman from './Rusty-hangman.png'
 export {
     Default,
     EyestrainManager,
+    IStrainManager,
     HFS,
     PersonalWebsite,
     PythonicHangman,
